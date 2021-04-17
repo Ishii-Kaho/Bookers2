a42ec0f7ac6022f2e1d47c88592a42811f93702a
Ishii-Kaho <kh141smallbear@gmail.com>
2021-04-17 03:05:42 +0000

    [finish]header-footer-top-about-login layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'home#top'
+  root to: 'homes#top'
   get 'about' => 'homes#about'
   resources :post_books, only: [:new, :create, :index, :show, :destroy]
+  resources :homes, only: [:new, :create, :index, :show, :destroy]
 end
