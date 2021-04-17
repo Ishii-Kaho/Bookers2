75b2c3262bc8ecb3d844c8c7b9a683ff57a3409a
Ishii-Kaho <kh141smallbear@gmail.com>
2021-04-17 15:10:12 +0000

    [finish]20210418

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,18 @@
 class BooksController < ApplicationController
+  def new
+  end
+
+  def crete
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @users = @user.post_books.page(params[:page]).reverse_order
+  end
+
+  def show
+  end
+
+  def destroy
+  end
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,2 +1,6 @@
 class HomesController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @post_books = @user.post_images.page(params[:page]).reverse_order
+  end
 end
