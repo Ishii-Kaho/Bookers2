a1f6d197b1817b67653ca23dee67857d58a67133
Ishii-Kaho <kh141smallbear@gmail.com>
2021-04-21 09:15:49 +0000

    [finish]access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,6 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-    flash[:notice] = "Signed out successfully."
     root_path
   end
 
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9f3d52a9a6820ae9f09e608cfb16c790ce37ffea995df2b2eee6116949202db96e84147e9861e8207d5866aa5b53778bd3a5d93cbe864e14b3caefa85fbbdbb6'
