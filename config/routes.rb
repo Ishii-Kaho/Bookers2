b5dfa95ecd662476acce8cef4a66baa506887095
Ishii-Kaho <kh141smallbear@gmail.com>
2021-05-04 14:31:31 +0000

    [start]follow-function

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+  
   resources :users, only: [:show, :edit, :index, :update]
+  # フォローする
+  post 'follow/:id' => 'relationships#create', as: 'follow'
+  # フォローを外す
+  post 'unfollow/:id' => 'relationships#destroy', as: 'unfollow'
 end
 
